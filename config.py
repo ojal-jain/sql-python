73a2975a22cd97d1e63b0fa19fc12ea166ff8faf
Ojal <ojaljain18@gmail.com>
2023-10-25 16:17:26 +0530

    my config removed

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://ojal-cosmosdb.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'MVuK3hp9Jw45W29SFkPOLBACpv6nk5yONVsl2mGgKYLd1snpPNg35yks29izonZklHdN2g2ecJJEACDbipAGGA=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
-}
